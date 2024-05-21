976fd6ecf95472cf208f007df1744dca16dde1cf
enk16 <sofiya.korotkaya@inbox.ru>
2024-05-21 09:23:15 +0300

    hh11

--- flsite.py
+++ flsite.py
@@ -1,8 +1,8 @@
 import pickle
 
 import numpy as np
-from flask import Flask, render_template, url_for, request
-
+from flask import Flask, render_template, url_for, request, jsonify
+from sklearn.metrics import r2_score
 app = Flask(__name__)
 
 menu = [{"name": "Лаба 1", "url": "p_knn"},
@@ -16,7 +16,7 @@ loaded_model_log = pickle.load(open('model/logistic_model', 'rb'))
 
 @app.route("/")
 def index():
-    return render_template('index.html', title="Лабораторные работы, выполненные ФИО", menu=menu)
+    return render_template('index.html', title="Лабораторные работы, выполненные Короткой С.Г.", menu=menu)
 
 
 @app.route("/p_knn", methods=['POST', 'GET'])
@@ -29,6 +29,7 @@ def f_lab1():
                            float(request.form['list3']),
                            float(request.form['list4'])]])
         pred = loaded_model_knn.predict(X_new)
+
         return render_template('lab1.html', title="Метод k -ближайших соседей (KNN)", menu=menu,
                                class_model="Это: " + pred)
 
@@ -82,6 +83,33 @@ def f_tree():
                                menu=menu,
                                class_model=f"Сложность предмета: {pred[0]}",)
 
+@app.route('/api', methods=['get'])
+def get_sort():
+    X_new = np.array(([[float(request.args.get('list1')),
+                           float(request.args.get('list2')),
+                           float(request.args.get('list3')),
+                           float(request.args.get('list4')),
+                           float(request.args.get('list5')),
+                           float(request.args.get('list6')),
+                           float(request.args.get('list7'))
+                           ]]))
+    pred = loaded_model_log.predict(X_new)
+    pred = str(pred[0])
+    return jsonify(sort=pred)
 
+@app.route('/api_v2', methods=['get'])
+def get_sort_v2():
+    request_data = request.get_json()
+    X_new = np.array([[float(request_data['list1']),
+                           float(request_data['list2']),
+                           float(request_data['list3']),
+                           float(request_data['list4']),
+                           float(request_data['list5']),
+                           float(request_data['list6']),
+                           float(request_data['list7'])
+                           ]])
+    pred = loaded_model_log.predict(X_new)
+
+    return jsonify(sort=pred)
 if __name__ == "__main__":
     app.run(debug=True)
